f096cafdcb0b05639b08d1e1e855e539c92b8fb6
DagmawitG <56536463+DagmawitG@users.noreply.github.com>
2022-04-14 19:59:12 +0300

    Time: 289 ms (44.14%), Space: 14.2 MB (79.28%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.py
+++ 547-number-of-provinces/547-number-of-provinces.py
@@ -6,50 +6,21 @@ class Solution:
         
         def findParent(node):
             if node != parent[node]:
-                return findParent(parent[node])
-            return node
+                parent[node] = findParent(parent[node])
+            return parent[node]
 
-        def union(node1,node2):
-            parent1 = findParent(node1)
-            parent2 = findParent(node2)
+        def union(node1, node2):
+            parent1, parent2 = findParent(node1), findParent(node2)
             if parent1 != parent2:
-                
                 parent1,parent2 = sorted([parent1,parent2], key= lambda x:child[x])
                 parent[parent1] = parent2
                 child[parent2] += child[parent1]
-                child[parent1] = 0
-                
-                
+        
         for i in range(n):
             for j in range(n):
-                if i!=j and isConnected[i][j]==1:
+                p1, p2 = findParent(i), findParent(j)
+                if i != j and (isConnected[i][j] or p1 == p2):
                     union(i,j)
-                    # print(child)
-        result = 0
-        for c in child:
-            result += int(bool(c))
-        return result
-                
-            
-            
-                
-        
-
-        
-        # visited = set()
-        # province = 0
-        # def dfs(city):
-        #     visited.add(city)
-        #     for i in range(len(isConnected[city])):
-        #         if isConnected[city][i] == 1 and i not in visited:
-        #             dfs(i)
-        # for i in range(len(isConnected)):
-        #     if i not in visited:
-        #         dfs(i)
-        #         province += 1
-        # return province
-        
-                
-            
-            
-        
+                    
+        result = set(parent)
+        return len(result)
