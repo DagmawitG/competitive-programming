d7869fc5a71cc51069efb980b345529a175d4996
DagmawitG <56536463+DagmawitG@users.noreply.github.com>
2022-05-14 22:19:20 +0300

    Time: 479 ms (88.61%), Space: 16 MB (76.55%) - LeetHub

---
+++ 743-network-delay-time/743-network-delay-time.py
@@ -0,0 +1,23 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        edges = collections.defaultdict(list)
+        for u,v,w in times:
+            edges[u].append((v,w))
+        
+        minHeap = [(0,k)] # initializing minheap with given source node 
+        visited = set()   # initializing a set to keep track of visited node
+        time = 0          # initial time delay 
+        while minHeap:
+            w1,n1 = heapq.heappop(minHeap)
+            if n1 in visited:
+                continue
+            visited.add(n1)
+            time = max(time,w1)
+            
+            for n2,w2 in edges[n1]: # applying bfs on all the neighbour node
+                if n2 not in visited:
+                    heapq.heappush(minHeap,(w1+w2,n2)) # if the neighbour not visited yet then
+                                                       # pushing it to heap and all along path value
+            
+        return time if len(visited) == n else -1
+        
