ae5e9ad8c18594b9475c3749576c894f528b6a0f
DagmawitG <56536463+DagmawitG@users.noreply.github.com>
2022-08-03 23:31:41 +0300

    Time: 486 ms (47.30%), Space: 16.9 MB (98.89%) - LeetHub

--- 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.py
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.py
@@ -4,34 +4,33 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def sizeNodes(self,head):
-        count=0
-        
-        while head :
-            head = head.next
-            count += 1
-        return count
+    def reverseList(self,head):
+        prev = None
+        current = head
+        while(current != None):
+            temp = current.next
+            current.next = prev
+            prev = current
+            current = temp
+        return prev
     def nextLargerNodes(self, head: Optional[ListNode]) -> List[int]:
+        result = []
         stack = []
-        temp = []
-        count = self.sizeNodes(head)
-        result = [0]*count
-       
-        while head:
-
-            while stack and temp[stack[-1]]<head.val:
-                result[stack.pop()] = head.val
-            temp.append(head.val)
-            stack.append(len(temp)-1)
-            head = head.next     
-           
-            
-        return result
-            
-            
+        head = self.reverseList(head)
         
-        
-    
-        
-        
+        while(head != None):
+            if(len(stack)==0):
+                result.append(0)
+               
+                stack.append(head.val)
+                head = head.next
+            elif(stack[-1] > head.val):
+                
+                result.append(stack[-1])
+                
+                stack.append(head.val)
+                head = head.next
+            else:
+                stack.pop()
+        return result[::-1]
         
