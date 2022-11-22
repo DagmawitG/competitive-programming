228531f18f0055df4c70fcbbc460fbb2da3abca9
DagmawitG <56536463+DagmawitG@users.noreply.github.com>
2022-11-22 20:23:11 +0300

    Time: 258 ms (64.72%), Space: 16.9 MB (77.81%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,15 +1,29 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        memo = [[len(word2) - i] * (len(word1)+1) for i in range(len(word2))]
-        memo.append([len(word1) - x for x in range(len(word1) + 1)])
-        for row in range(len(memo)-2,-1,-1):
-            for col in range(len(memo[0])-2,-1,-1):
-                if word2[row] == word1[col]:
-                    memo[row][col] = memo[row+1][col+1]
-                else:
-                    insert = memo[row][col+1] 
-                    delete = memo[row+1][col] 
-                    replace = memo[row+1][col+1] 
-                    memo[row][col] = 1 + min(insert,delete,replace)
-        return memo[0][0]
+        memo = {}
+        def dp(i,j):   
+            if i == len(word1):
+                return len(word2)- j
+            if j == len(word2):
+                return len(word1) - i
+            if i ==len(word1) and j == len(word2):
+                return 0
+                
+            if (i,j) in memo:
+                return memo[(i,j)]
+          
+            if word1[i] == word2[j]:
+                memo[(i,j)] = dp(i+1,j+1)
+            else:
+                insert = dp(i,j+1)
+                delete = dp(i+1,j)
+                replace = dp(i+1,j+1)
+            
+                memo[(i,j)] = 1 + min(insert,delete,replace)
+            
+            return memo[(i,j)]
+        
+        return dp(0,0)
+            
+            
         
