96070ea97659f8875fb2d412e9fc87ba7454a040
DagmawitG <56536463+DagmawitG@users.noreply.github.com>
2022-11-23 11:23:15 +0300

    Time: 805 ms (32.78%), Space: 15.7 MB (34.21%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -4,18 +4,14 @@ class Solution:
         atlantic = set()
         self.rows= len(heights)
         self.cols = len(heights[0])
-        '''
-        pacific - row - (0 - rows - 1) and col = 0
-        atlantic - row - (rows -1) and col = (0,cols - 1)
-        '''
         
         for r in range(self.rows):
-            self.bfs(r,0,heights,pacific)
-            self.bfs(r,self.cols-1,heights,atlantic)
+            self.dfs(r,0,heights,pacific)
+            self.dfs(r,self.cols-1,heights,atlantic)
             
         for c in range(self.cols):
-            self.bfs(0,c,heights,pacific)
-            self.bfs(self.rows-1,c,heights,atlantic)
+            self.dfs(0,c,heights,pacific)
+            self.dfs(self.rows-1,c,heights,atlantic)
             
         return pacific & atlantic
             
@@ -24,20 +20,18 @@ class Solution:
         return 0 <= row < self.rows and 0 <= col < self.cols
         
         
-    def bfs(self,r,c,heights,visited):
-        queue = deque([(r,c)])
-        visited.add((r,c))
+    def dfs(self,row,col,heights,visited):
+        visited.add((row,col))
         direction = [(0,1),(1,0),(-1,0),(0,-1)]
         
-        while queue:
-            row,col = queue.popleft()
-            for i,j in direction:
-                new_row = row + i
-                new_col = col + j
-                if self.inbound(new_row,new_col) and (new_row,new_col) not in visited and heights[row][col] <= heights[new_row][new_col]:
-                    queue.append((new_row,new_col))
-                    visited.add((new_row,new_col))
-                    
+       
+        for i,j in direction:
+            new_row = row + i
+            new_col = col + j
+            if self.inbound(new_row,new_col) and (new_row,new_col) not in visited and heights[row][col] <= heights[new_row][new_col]:
+                self.dfs(new_row,new_col,heights,visited)
+                visited.add((new_row,new_col))
+
                 
             
         
