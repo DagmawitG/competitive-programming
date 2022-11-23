0e2f0330b48491f980ce9cfb23efb063151b7496
DagmawitG <56536463+DagmawitG@users.noreply.github.com>
2022-11-23 16:18:44 +0300

    Time: 224 ms (90.16%), Space: 20.7 MB (21.83%) - LeetHub

---
+++ 1236-web-crawler/1236-web-crawler.py
@@ -0,0 +1,36 @@
+# """
+# This is HtmlParser's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class HtmlParser(object):
+#    def getUrls(self, url):
+#        """
+#        :type url: str
+#        :rtype List[str]
+#        """
+
+class Solution:
+    def crawl(self, startUrl: str, htmlParser: 'HtmlParser') -> List[str]:
+        
+        domainName = startUrl.split("http://")[1].split('/')[0]
+        visited = {startUrl}
+        answer = [startUrl]
+        def bfs(url):
+            queue = deque([(url)]) 
+            while queue:
+                
+                url = queue.popleft()
+                getHtmlUrls = htmlParser.getUrls(url)
+                for currentUrl in getHtmlUrls:
+                    if currentUrl not in visited and currentUrl.split("http://")[1].split('/')[0] == domainName:
+                        answer.append(currentUrl)
+                        visited.add(currentUrl)
+                        queue.append(currentUrl)
+                        
+                    else:
+                        continue
+        bfs(startUrl)
+        return answer
+    
+        
+        
