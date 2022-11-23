40ef40e8edd7f73b211847eecdecf19f863f206e
DagmawitG <56536463+DagmawitG@users.noreply.github.com>
2022-11-23 10:27:09 +0300

    Time: 650 ms (64.26%), Space: 16.6 MB (51.02%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,44 +1,71 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        row = len(grid)
-        col = len(grid[0])
-        direction = [(0,1),(1,0),(-1,0),(0,-1)]
-        visited = set()
-        def isValid(r,c):
-            return 0 <= r < row and 0 <= c < col
-        
-        def dfs(r,c):
-            grid[r][c]='0'
-            for x,y in direction:
-                nr = r + x
-                nc = c + y
-                if isValid(nr,nc) and grid[nr][nc] == '1':
-                    dfs(nr,nc)
-                    grid[nr][nc]='0'
-                         
-                    
-        
-        islands = 0                
-        for r in range(row):
-            for c in range(col):
-                if grid[r][c] == '1':
-                    dfs(r,c)
+        self.m = len(grid)
+        self.n = len(grid[0]) 
+        islands = 0
+        for row in range(self.m):
+            for col in range(self.n):
+                if grid[row][col] == '1':
+                    self.dfs(row,col,grid)
                     islands += 1
                 
+                    
         return islands
                     
-                    
-        
+      
+    def inbound(self,r,c):
+        return 0 <= r < self.m and 0 <= c < self.n
+    
+    
+    def dfs(self,row,col,grid):
+        direction = [(0,1),(1,0),(0,-1),(-1,0)]
+        grid[row][col] = '0'
+        for x,y in direction:
+            new_row = row + x
+            new_col = col + y
+
+            if self.inbound(new_row, new_col) and  grid[new_row][new_col] == '1': 
+                self.dfs(new_row,new_col,grid)
+                grid[new_row][new_col] = '0'
                 
-            
-            
-        
-       
-                    
-                    
-                 
                 
-            
-            
-            
+                    
+                    
+    
         
+
+'''
+        
+  ["1","1","1","1","0"]
+  ["1","1","0","1","0"]
+  ["1","1","0","0","0"]
+  ["0","0","0","0","0"]
+    
+    
+
+    
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+
+    number of islands = 3
+    
+    
+    cases
+    - if all values in the grid are 1s or if it's all a land
+    - if all values are 0s, if it's only water
+    - if we have 1 island 
+    - islands > 1
+    
+    check
+    - if it's inbound
+    - visited
+    - and the grid[m][n] == 1
+    
+    BFS or DFS 
+    Time complexity = O(m*n)
+    Space complexity = O(N) - visited dicitonary to track the visited values
+    
+'''
+    
