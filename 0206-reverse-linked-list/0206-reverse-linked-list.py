7d5646eb5d4aa41b5686414c6417ed62075ef24b
DagmawitG <56536463+DagmawitG@users.noreply.github.com>
2022-11-26 12:17:57 +0300

    Time: 49 ms (77.93%), Space: 20.4 MB (8.39%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -8,14 +8,16 @@ import sys
 sys.setrecursionlimit(10**6)
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        current = head
-        previous = None
-        while current:
-            nxtPtr = current.next
-            current.next = previous
-            previous = current
-            current = nxtPtr
-        return previous
+        if not head:
+            return None
+        newHead = head
+        if head.next:
+            newHead = self.reverseList(head.next)
+            head.next.next = head
+        head.next = None
+        return newHead
+            
+        
         
         
             
