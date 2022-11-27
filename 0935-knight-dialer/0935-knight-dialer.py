856c44ece57cdd6808fb220c594b80ce37fb57d0
DagmawitG <56536463+DagmawitG@users.noreply.github.com>
2022-11-27 13:07:08 +0300

    Time: 7319 ms (6.70%), Space: 44.8 MB (17.16%) - LeetHub

--- 0935-knight-dialer/0935-knight-dialer.py
+++ 0935-knight-dialer/0935-knight-dialer.py
@@ -1,41 +1,45 @@
 class Solution:
-
-    
     def __init__(self):
-        self.mat = [[1, 2, 3], [4,5,6], [7,8,9], ['*', 0, '#']]
-        self.nrows = len(self.mat)
-        self.ncols = len(self.mat[0])
-        self.in_valid = set([(3,0), (3,2)])
-    
-    def valid_jumps(self, row, col):
-        valid = []
-        for r_inc, c_inc in [(2, 1), (2, -1), (-2, 1), (-2, -1), (1, 2), (-1, 2), (1,-2), (-1, -2)]:
-            n_row, n_col = row + r_inc, col + c_inc
-            if 0<= n_row < self.nrows and 0 <= n_col < self.ncols and (n_row, n_col) not in self.in_valid:
-                valid.append((n_row, n_col))
-        return valid
-    
-    def recursive_dialer(self, row, col, jumps, cache):
-        if jumps == 0: return 1
-        if (row,col, jumps) in cache: return cache[(row, col, jumps)]
-        max_number = 0
+        self.matrix = [[1,2,3],[4,5,6],[7,8,9],['*',0,'#']]
+        self.rows = len(self.matrix)
+        self.cols = len(self.matrix[0])
+        self.inValidRC = set([(3,0),(3,2)])
         
-        for n_row, n_col in self.valid_jumps(row, col):
-            ret = self.recursive_dialer(n_row, n_col, jumps-1, cache)
-            max_number += ret
+    def validMoves(self,row,col):
+        direction = [(2,1),(2,-1),(-2,1),(-2,-1),(1,2),(-1,2),(-1,-2),(1,-2)]
+        validJumps = [] 
+        for r,c in direction:
+            new_row = row + r
+            new_col = col + c
+            if 0 <= new_row < self.rows and 0 <= new_col < self.cols and (new_row,new_col) not in self.inValidRC:
+                validJumps.append((new_row,new_col))
+        return validJumps
+                
+    def recursionKnightDialer(self,row,col,n,cache):
+        distinctNumber = 0
+        if n == 0:
+            return 1
         
-        cache[(row, col, jumps)] = max_number
+        if (row,col,n) in cache:
+            return cache[(row,col,n)]
         
-        return cache[(row, col, jumps)]
-    
-    def knightDialer(self, n: int) -> int:
-        max_number = 0
-        if n == 0: return max_number
+        for (new_row,new_col) in self.validMoves(row,col):
+            distinctNumber += self.recursionKnightDialer(new_row,new_col,n-1,cache)
+            
+        cache[(row,col,n)] = distinctNumber
+        return cache[(row,col,n)]
+        
+                                          
+    def knightDialer(self, n: int) -> int:      
+        distinctNumber = 0
+        if n == 0:
+            return distinctNumber
         cache = {}
+        for row in range(self.rows):
+            for col in range(self.cols):
+                if (row,col) not in self.inValidRC:
+                    distinctNumber += self.recursionKnightDialer(row,col,n-1,cache)
+        return distinctNumber % (10**9 + 7)
         
-        for row in range(self.nrows):
-            for col in range(self.ncols):
-                if (row, col) not in self.in_valid:
-                    max_number += self.recursive_dialer(row, col, n-1, cache)
         
-        return max_number % (10**9 + 7)
+        
