1f498b9f187c9edde1c91c33b6e03a9e89995260
DagmawitG <56536463+DagmawitG@users.noreply.github.com>
2022-11-29 09:57:24 +0300

    Time: 47 ms (86.41%), Space: 13.8 MB (98.61%) - LeetHub

---
+++ 1029-two-city-scheduling/1029-two-city-scheduling.py
@@ -0,0 +1,26 @@
+class Solution:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        costs = sorted(costs,key=lambda x: x[1]-x[0])
+        n = len(costs)//2
+        costB = 0
+        costA = 0
+        for i in range(n):
+            costB += costs[i][1]
+        for i in range(n,len(costs)):
+            costA += costs[i][0]
+            
+        return costA + costB
+        
+            
+        
+        
+        
+        
+        ''' 0        1          2       3
+         [[10,20],[30,200],[40,50],[10,20]]
+         
+         person 0 = min(10,20) =  10
+         
+         
+        
+        '''
